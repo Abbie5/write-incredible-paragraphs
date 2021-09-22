76aabd7abe7faf5a5ab45b32e585f92805c712f0
Luna <luna@lunaluna.me>
2021-09-23 00:14:39 +0100

    Added a BasicTextField to allow for simple text editing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,14 @@
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 fun main(args: Array<String>) = application {
     Window(onCloseRequest = ::exitApplication) {
-        // Start here
+        var text by remember { mutableStateOf("edit here") }
+        BasicTextField(text, { text = it })
     }
 }
