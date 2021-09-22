a85bee056de8f6a981a2ba1242a90f1a0a751a97
Luna <luna@lunaluna.me>
2021-09-23 00:19:54 +0100

    Extracted Editable Text as a separate function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,16 @@
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        var text by remember { mutableStateOf("edit here") }
-        BasicTextField(text, { text = it })
+        EditableText("edit here!")
     }
+}
+
+@Composable
+fun EditableText(value: String = "") {
+    var text by remember { mutableStateOf(value) }
+    BasicTextField(text, { text = it })
 }
